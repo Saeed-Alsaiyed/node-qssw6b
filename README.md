4d0135d743c0b333e07176845ffe2d50a540ef65
Saeed-Alsaiyed <46853266+Saeed-Alsaiyed@users.noreply.github.com>
2022-10-08 13:43:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-qssw6b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-qssw6b)
---
+++ index.js
@@ -0,0 +1,12 @@
+// run `node index.js` in the terminal
+const numbers = [102, 91, 3, -6, 203, 8, 33, 21, 150, 77, 61, 3, 91, 21, 8];
+
+function total(arr) {
+  const sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+console.log(total(numbers));
